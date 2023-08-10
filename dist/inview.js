7b7b56a6fa2af3a15579aee8ca1b2446283195e0
Obaydur Rahman <obayed.opu@gmail.com>
2023-08-10 19:59:30 +0600

    Build version 2.0.0

--- dist/inview.js
+++ dist/inview.js
@@ -1,145 +1,133 @@
-/**
- * InView
- *
- * Check if element is visible in viewport
- */
-export default class InView {
+class n {
   /**
-   * InView: Elements to observe
+   * Constructor
    *
-   * @param {String} selector
-   * @returns {Object} this object to chain .on() methods.
-   */
-  constructor(selector) {
-    if (selector === "document" || selector === "window") {
-      console.error("InView: Invalid selector.");
-    } else {
-      this.items = document.querySelectorAll(selector);
-    }
-
-    this.paused = false;
-
-    this.delay = 0;
-
-    this.threshold = [];
-    for (let i = 0; i <= 1; i += 0.01) {
-      this.threshold.push(i);
-    }
-
-    return this;
-  }
-
-  /**
-   * InView: Pause observing
-   * @returns {Object} this object to chain .on() methods.
+   * Create a new InView instance
+   *
+   * @param {InViewConfig | string} config - Configuration object or CSS selector
+   *
    * @example
-   * new InView(".selector").pause();
-   * new InView(".selector").pause().on("enter", () => {});
-   * new InView(".selector").pause().on("exit", () => {});
+   * new InView(".selector");
+   *
+   * @example
+   * new InView({
+   *   selector: ".selector",
+   *   delay: 1000,
+   *   precision: "low",
+   *   single: true
+   * });
+   */
+  constructor(e) {
+    this.items = null, this.paused = !1, this.delay = 0, this.threshold = [], this.single = !1;
+    let s = 0.01;
+    typeof e == "string" ? (this.items = document.querySelectorAll(e), this.delay = 0) : typeof e == "object" && (e.delay && (this.delay = e.delay), e.single && (this.single = e.single), e.precision === "low" ? s = 0.1 : e.precision === "medium" ? s = 0.01 : e.precision === "high" && (s = 1e-3), this.single ? this.items = document.querySelector(e.selector) : this.items = document.querySelectorAll(e.selector));
+    for (let i = 0; i <= 1; i += s)
+      this.threshold.push(i);
+  }
+  /**
+   * Pause the observer
+   *
+   * @returns {InView} - Returns the InView instance
+   *
+   * @example
+   * const inview = new InView(".selector");
+   * inview.on("enter", (e) => {});
+   * // pause on specific needs
+   * inview.pause();
    */
   pause() {
-    this.paused = true;
-    return this;
+    return this.paused = !0, this;
   }
-
   /**
-   * InView: Resume observing
-   * @returns {Object} this object to chain .on() methods.
+   * Resume the observer
+   *
+   * @returns {InView} - Returns the InView instance
+   *
    * @example
-   * new InView(".selector").pause().resume().on("enter", () => {});
-   * new InView(".selector").pause().resume().on("exit", () => {});
+   * const inview = new InView(".selector");
+   * inview.on("enter", (e) => {});
+   * // pause the observer
+   * inview.pause();
+   * // resume the observer again
+   * inview.resume();
    */
   resume() {
-    this.paused = false;
-    return this;
+    return this.paused = !1, this;
   }
-
   /**
-   * InView: Set delay
-   * @param {Number} delay
-   * @returns {Object} this object to chain .on() methods.
+   * Set the delay
+   *
+   * @param {number} delay - Delay in ms
+   *
+   * @returns {InView} - Returns the InView instance
+   *
    * @example
-   * new InView(".selector").delay(1000).on("enter", () => {});
-   * new InView(".selector").delay(1000).on("exit", () => {});
+   * const inview = new InView(".selector");
+   * inview.on("enter", (e) => {});
+   * // set delay to 1000ms
+   * inview.setDelay(1000);
    */
-  setDelay(delay) {
-    this.delay = delay;
-    return this;
+  setDelay(e) {
+    return this.delay = e, this;
   }
-
   /**
-   * InView: Event Listener
-   * @param {String} event name of the event (enter or exit)
-   * @param {Function} callback callback function with event parameter
-   * @returns {Object} return this object to chain .on() methods.
+   * Listen for enter or exit events
+   *
+   * @param {"enter" | "exit"} event - Event type
+   * @param {CallableFunction} callback - Callback function
+   *
+   * @returns {InView} - Returns the InView instance
+   *
    * @example
-   * new InView(".selector").on("enter", () => {});
-   * new InView(".selector").on("exit", () => {});
-   * new InView(".selector").on("enter", () => {}).on("exit", () => {});
+   * const inview = new InView({...});
+   * inview.on("enter", (e: InViewEvent) => {
+   *  console.log(e.percentage);
+   * });
+   *
+   * inview.on("exit", (e: InViewEvent) => {
+   *  console.log("exit");
+   * });
+   *
+   * @example
+   * new InView(".selector").on("enter", (e: InViewEvent) => {
+   *  console.log(e.percentage);
+   * }).on("exit", (e: InViewEvent) => {
+   *  console.log("exit");
+   * });
    */
-  on(event, callback) {
-    if (event === "enter" || event === "exit") {
-      /**
-       * Check if IntersectionOberver is available
-       */
-      if ("IntersectionObserver" in window) {
-        /**
-         * New observer to check for each items position
-         */
-        var observer = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              /**
-               * Determine if element exited or entered the viewport
-               */
-              if (
-                (event === "enter" && entry.intersectionRatio > 0) ||
-                (event === "exit" && entry.intersectionRatio === 0)
-              ) {
-                /**
-                 * Create output object
-                 */
-                let e = {
-                  percentage: entry.intersectionRatio * 100,
-                  rootBounds: entry.rootBounds,
-                  boundingClientRect: entry.boundingClientRect,
-                  intersectionRect: entry.intersectionRect,
-                  target: entry.target,
-                  time: entry.time,
-                  event: event,
-                };
-
-                /**
-                 * Call the callback function if not paused and if delay is over
-                 */
-                if (!this.paused) {
-                  if (this.delay > 0) {
-                    setTimeout(() => {
-                      callback(e);
-                    }, this.delay);
-                  } else {
-                    callback(e);
-                  }
-                }
-              }
-            });
-          },
-          {
-            threshold: this.threshold,
-          }
-        );
-
-        this.items.forEach((item) => {
-          // observe each item
-          observer.observe(item);
-        });
-      } else {
-        console.error("InView: IntersectionObserver not supported.");
-      }
-    } else {
-      console.error("InView: Event name " + event + " is invalid.");
-    }
-
+  on(e, s) {
+    if ("IntersectionObserver" in window) {
+      var i = new IntersectionObserver(
+        (r) => {
+          r.forEach((t) => {
+            if (e === "enter" && t.intersectionRatio > 0 || e === "exit" && t.intersectionRatio === 0) {
+              let o = {
+                percentage: t.intersectionRatio * 100,
+                rootBounds: t.rootBounds,
+                boundingClientRect: t.boundingClientRect,
+                intersectionRect: t.intersectionRect,
+                target: t.target,
+                time: t.time,
+                event: e
+              };
+              this.paused || (this.delay > 0 ? setTimeout(() => {
+                s(o);
+              }, this.delay) : s(o));
+            }
+          });
+        },
+        {
+          threshold: this.threshold
+        }
+      );
+      this.items instanceof Element ? i.observe(this.items) : this.items instanceof NodeList ? this.items.forEach((r) => {
+        i.observe(r);
+      }) : console.error("InView: No items found.");
+    } else
+      console.error("InView: IntersectionObserver not supported.");
     return this;
   }
 }
+export {
+  n as default
+};
